08561c6789dc67474b0149b960e5596e1dc4453b
kusanagi.y <kusanagi.y@mujina-prod.com>
2022-04-10 18:23:53 +0900

    initial commit

---
+++ initial-setup.bat
@@ -0,0 +1,95 @@
+@REM Install apps. below are on hand
+@REM -----
+@REM Ableton Live
+@REM GameSynth Tool
+@REM Node JS
+@REM Office Excel, Word
+@REM MAYA44 eX Driver
+@REM PureRef
+@REM Xbox (Game Pass)
+@REM Twitter (PWA)
+@REM Pinterest (PWA)
+@REM -----
+
+@REM Chololatey
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+@REM WSL2
+wsl --install
+
+@REM Brave Browser
+choco install brave -y
+
+@REM Chrome
+choco install googlechrome -y
+
+@REM 7Zip
+choco install 7zip -y
+
+@REM Adobe Creative Cloud
+choco install adobe-creative-cloud -y
+
+@REM Blender
+choco install blender -y
+
+@REM Davinci Resolve
+
+@REM Discord
+choco install discord -y
+
+@REM Docker Desktop
+choco install docker-desktop -y
+
+@REM Draw.io
+choco install drawio -y
+
+@REM Epic Games Launcher
+choco install epicgameslauncher -y
+
+@REM Git
+choco install git -y
+
+@REM Google Drive
+choco install google-drive-file-stream -y
+
+@REM Honey View
+choco install honeyview.install -y
+
+@REM iTunes
+choco install itunes -y
+
+@REM Microsoft VSCode
+choco install vscode -y
+
+@REM Mozilla Firefox
+choco install firefox -y
+
+@REM MPC-BE x64
+choco install mpc-be -y
+
+@REM NVIDIA Geforce Experience
+choco install geforce-experience -y
+
+@REM OBS Studio
+choco install obs-studio -y
+
+@REM QNAP Finder Pro
+choco install qfinderpro -y
+
+@REM Slack
+choco install slack -y
+
+@REM Steam
+choco install steam-client -y
+
+@REM Toggl Track
+choco install toggl -y
+
+@REM Unity Hub
+choco install unity-hub -y
+
+@REM Windows Terminal
+choco install microsoft-windows-terminal -y
+
+@REM Logicool SetPoint
+choco install setpoint -y
